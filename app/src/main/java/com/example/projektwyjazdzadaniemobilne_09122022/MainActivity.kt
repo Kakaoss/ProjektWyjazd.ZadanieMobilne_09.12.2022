2dcc017c5d3dbb258d994aa9acb602d82a7f02bf
IETI <58682773+marcinkolacz@users.noreply.github.com>
2022-12-09 16:31:44 +0100

    ustawiona minimalna i max data, dodanie poczatku wyjazdu o konca wyjazdu(narazie nie dziala tylko wyglad)

--- app/src/main/java/com/example/projektwyjazdzadaniemobilne_09122022/MainActivity.kt
+++ app/src/main/java/com/example/projektwyjazdzadaniemobilne_09122022/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.projektwyjazdzadaniemobilne_09122022
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.CalendarView
@@ -8,13 +9,21 @@ import android.widget.TextView
 class MainActivity : AppCompatActivity() {
     lateinit var calendar: CalendarView
     lateinit var iloscdni: TextView
+    lateinit var pocz_wyj: TextView
+    lateinit var koniec_wyj: TextView
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        pocz_wyj = findViewById(R.id.poczatek_wyjazdu)
+        koniec_wyj = findViewById(R.id.koniec_wyjazdu)
         iloscdni = findViewById(R.id.textDlWyjazdu)
         calendar = findViewById(R.id.calendar)
 
+        calendar.setOnClickListener {
 
+
+        }
     }
 }
