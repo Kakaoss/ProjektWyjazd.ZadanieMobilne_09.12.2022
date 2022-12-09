a775dafe8cc2e966b2032140c518c04b53cab88f
IETI <58682773+marcinkolacz@users.noreply.github.com>
2022-12-09 15:35:02 +0100

    dodanie zmiennych

--- app/src/main/java/com/example/projektwyjazdzadaniemobilne_09122022/MainActivity.kt
+++ app/src/main/java/com/example/projektwyjazdzadaniemobilne_09122022/MainActivity.kt
@@ -2,12 +2,18 @@ package com.example.projektwyjazdzadaniemobilne_09122022
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.CalendarView
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var calendar: CalendarView
+    lateinit var iloscdni: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        iloscdni = findViewById(R.id.textDlWyjazdu)
+        calendar = findViewById(R.id.calendar)
 
 
     }
